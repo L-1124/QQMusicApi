6110a21b18b91a7b5d1370d2ca026876c4200fec
Luren <68656403+luren-dc@users.noreply.github.com>
2025-12-27 23:46:02 +0800

    🧹 chore(release): release 0.4.0

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -6,7 +6,7 @@ from . import album, comment, login, lyric, mv, search, singer, song, songlist, 
 from .utils.credential import Credential
 from .utils.session import Session, get_session, set_session
 
-__version__ = "0.3.6"
+__version__ = "0.4.0"
 
 logger = logging.getLogger("qqmusicapi")
 
