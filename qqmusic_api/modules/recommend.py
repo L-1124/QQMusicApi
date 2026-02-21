3e6f0732df23095e1993ef57b1a369a801144518
Luren <68656403+luren-dc@users.noreply.github.com>
2026-02-21 18:30:09 +0800

    ✨ feat(modules): 迁移推荐模块

---
+++ qqmusic_api/modules/recommend.py
@@ -0,0 +1,54 @@
+"""推荐模块。"""
+
+from typing import TYPE_CHECKING
+
+from ._base import ApiModule
+
+if TYPE_CHECKING:
+    from ..core.request import Request
+
+
+class RecommendApi(ApiModule):
+    """推荐 API。"""
+
+    def get_home_feed(self) -> "Request[dict]":
+        """获取主页推荐。"""
+        data = {
+            "direction": 0,
+            "page": 1,
+            "s_num": 0,
+        }
+        return self._client.build_request("music.recommend.RecommendFeed", "get_recommend_feed", data)
+
+    def get_guess_recommend(self) -> "Request[dict]":
+        """获取猜你喜欢。"""
+        data = {
+            "id": 99,
+            "num": 5,
+            "from": 0,
+            "scene": 0,
+            "song_ids": [],
+            "ext": {"bluetooth": ""},
+            "should_count_down": 1,
+        }
+        return self._client.build_request("music.radioProxy.MbTrackRadioSvr", "get_radio_track", data)
+
+    def get_radar_recommend(self) -> "Request[dict]":
+        """获取雷达推荐。"""
+        data = {
+            "Page": 1,
+            "ReqType": 0,
+            "FavSongs": [],
+            "EntranceSongs": [],
+        }
+        return self._client.build_request("music.recommend.TrackRelationServer", "GetRadarSong", data)
+
+    def get_recommend_songlist(self) -> "Request[dict]":
+        """获取推荐歌单。"""
+        data = {"From": 0, "Size": 25}
+        return self._client.build_request("music.playlist.PlaylistSquare", "GetRecommendFeed", data)
+
+    def get_recommend_newsong(self) -> "Request[dict]":
+        """获取推荐新歌。"""
+        data = {"type": 5}
+        return self._client.build_request("newsong.NewSongServer", "get_new_song_info", data)
