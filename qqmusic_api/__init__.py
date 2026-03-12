a36398834923a72a40d1416fc0e79186a595fa50
Luren <68656403+luren-dc@users.noreply.github.com>
2026-03-12 22:32:35 +0800

    ♻️ refactor(api): 收敛根入口导出并保留必要异常

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -1,17 +1,16 @@
 """QQMusic API 公开入口."""
 
-from .algorithms import qrc_decrypt, sign_request
 from .core.client import Client
 from .core.exceptions import (
     ApiError,
-    BaseError,
     HTTPError,
+    LoginError,
     LoginExpiredError,
     NetworkError,
     NotLoginError,
+    RateLimitError,
     SignInvalidError,
 )
-from .core.request import Request, RequestGroup
 from .core.versioning import Platform
 from .models.request import Credential
 
@@ -19,18 +18,15 @@ __version__ = "0.5.0"
 
 __all__ = [
     "ApiError",
-    "BaseError",
     "Client",
     "Credential",
     "HTTPError",
+    "LoginError",
     "LoginExpiredError",
     "NetworkError",
     "NotLoginError",
     "Platform",
-    "Request",
-    "RequestGroup",
+    "RateLimitError",
     "SignInvalidError",
     "__version__",
-    "qrc_decrypt",
-    "sign_request",
 ]
