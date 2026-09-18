e2a1fee9c7a3d5193a3b2a37b6f6b4a6bf5348de
Luren <68656403+luren-dc@users.noreply.github.com>
2026-09-18 13:38:10 +0800

    ♻️ refactor(top): 迁移 TopApi 端点至 @cgi_endpoint 并更新 Web 路由

--- qqmusic_api/modules/top.py
+++ qqmusic_api/modules/top.py
@@ -1,5 +1,6 @@
 """排行榜相关 API."""
 
+from ..core.endpoint import CgiRequestData, cgi_endpoint
 from ..core.pagination import OffsetStrategy
 from ..models.top import TopCategoryResponse, TopDetailResponse
 from ._base import ApiModule
@@ -8,15 +9,22 @@ from ._base import ApiModule
 class TopApi(ApiModule):
     """排行榜相关 API."""
 
-    def get_category(self):
+    @cgi_endpoint(
+        key="top.get_category",
+        module="music.musicToplist.Toplist",
+        method="GetAll",
+        response_model=TopCategoryResponse,
+    )
+    def get_category(self) -> CgiRequestData:
         """获取所有排行榜分类."""
-        return self._build_cgi(
-            module="music.musicToplist.Toplist",
-            method="GetAll",
-            param={},
-            response_model=TopCategoryResponse,
-        )
+        return CgiRequestData(param={})
 
+    @cgi_endpoint(
+        key="top.get_detail",
+        module="music.musicToplist.Toplist",
+        method="GetDetail",
+        response_model=TopDetailResponse,
+    )
     def get_detail(
         self,
         top_id: int,
@@ -24,7 +32,7 @@ class TopApi(ApiModule):
         page: int = 1,
         *,
         tag: bool = True,
-    ):
+    ) -> CgiRequestData:
         """获取排行榜详情及其歌曲列表.
 
         Args:
@@ -41,16 +49,14 @@ class TopApi(ApiModule):
         if tag:
             param["withTags"] = True
 
-        return self._build_cgi(
-            module="music.musicToplist.Toplist",
-            method="GetDetail",
+        return CgiRequestData(
             param=param,
             preserve_bool=tag,
-            response_model=TopDetailResponse,
             pager_strategy=OffsetStrategy[TopDetailResponse](
                 offset_key="offset",
                 page_size_key="num",
                 total_extractor=lambda r: r.info.total_num,
                 count_extractor=lambda r: len(r.songs),
             ),
-        ).with_extractor(lambda r: r.songs)
+            items_extractor=lambda r: r.songs,
+        )
--- web/src/routes/top.py
+++ web/src/routes/top.py
@@ -1,17 +1,15 @@
 """排行榜 Web 路由契约."""
 
-from qqmusic_api.models.top import TopCategoryResponse, TopDetailResponse
+from qqmusic_api.modules.top import TopApi
 
 from ..routing.route_types import PUBLIC_60, PUBLIC_300, WebRoute
 from ._helpers import TOP_DETAIL_OPTIONS, TOP_ID, R
 
 ROUTES: tuple[WebRoute, ...] = (
-    R("top", "get_category", "/top/get_category", TopCategoryResponse, cache=PUBLIC_300),
+    R(TopApi.get_category, "/top/get_category", cache=PUBLIC_300),
     R(
-        "top",
-        "get_detail",
+        TopApi.get_detail,
         "/top/{top_id}/detail",
-        TopDetailResponse,
         params=(*TOP_ID, *TOP_DETAIL_OPTIONS),
         cache=PUBLIC_60,
     ),
